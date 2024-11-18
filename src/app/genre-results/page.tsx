3d70dac97195978f68e4629967c91d3fb33af7c2
Felipev <felipevalbuena2017@gmail.com>
2024-11-18 11:02:50 -0500

    fix test1

--- src/app/genre-results/page.tsx
+++ src/app/genre-results/page.tsx
@@ -9,8 +9,7 @@ import 'react-circular-progressbar/dist/styles.css';
 import styles from './GenreResults.module.css'; // Import CSS module for styling
 import MainBanner from '../components/Header/MainBanner';
 import FilterBar from '../components/FilterBar';
-import MovieComponent from '../components/MovieComponent';
-import { Button, Col, Container, Row } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 const GenreResults = () => {
   interface Movie {
@@ -32,7 +31,6 @@ const GenreResults = () => {
   const router = useRouter();
 
   useEffect(() => {
-    setMovies([]);
     const fetchMovies = async () => {
       setLoading(true);
       try {
@@ -54,44 +52,9 @@ const GenreResults = () => {
     };
 
     if (genreId) {
-      console.log('fetching movies for genre:', genreId);
       fetchMovies();
     }
-  }, [genreId]);
-
-  useEffect(() => {
-    const fetchMovies = async () => {
-      setLoading(true);
-      try {
-        const response = await axios.get(`https://api.themoviedb.org/3/discover/movie`, {
-          headers: {
-            Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_BEARER_TOKEN}`,
-          },
-          params: {
-            with_genres: genreId,
-            page,
-          },
-        });
-        setMovies(prevMovies => [...prevMovies, ...response.data.results]);
-      } catch (error) {
-        console.error('Error fetching movies:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    if (genreId) {
-      console.log('fetching movies for genre:', genreId);
-      fetchMovies();
-    }
-  }, [page]);
-
-  useEffect(() => {
-
-    
-
-  }
-  , [movies]);
+  }, [genreId, page]);
 
   const handleSearch = (term) => {
     setSearchTerm(term);
@@ -112,39 +75,48 @@ const GenreResults = () => {
   return (
     <>
       <MainBanner />
-
-      <Container className='bg-gray' style={{color: 'white'}}>
-      <Row>
-        <Col xs lg="2" className='pt-5'>
-          <FilterBar onSearch={handleSearch} onGenreSelect={handleGenreSelect} />
-        </Col>
-        <Col lg='10' className='pt-5 p-5'>
-        <div>
-          <h1>Movies in Genre {genreId}</h1>
-          <div className={styles.grid}>
-            {movies.map(movie => (
-              <MovieComponent key={movie.id} movie={movie} />
-
-            ))}
-          </div>
-          {loading ? (
-            <p>Loading...</p>
-          ) : (
-            <div className='d-flex justify-content-center m-5'>
-              <Button onClick={handleLoadMore} className={`${styles.loadMoreButton} text-center warning`} variant='warning'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-clockwise" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z"/>
-            <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466"/>
-          </svg>
-          Load More
-            
-          </Button>
+      <FilterBar onSearch={handleSearch} onGenreSelect={handleGenreSelect} />
+      <div>
+        <h1>Movies in Genre {genreId}</h1>
+        <div className={styles.grid}>
+          {movies.map(movie => (
+            <div key={movie.id} className={styles.card} onClick={() => handleMovieClick(movie.id)}>
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={movie.title}
+                className={styles.image}
+              />
+              <h3>{movie.title}</h3>
+              <div style={{ width: 50, height: 50 }}>
+                <CircularProgressbar
+                  value={Math.ceil(movie.vote_average * 10)}
+                  text={`${Math.ceil(movie.vote_average * 10)}%`}
+                  styles={buildStyles({
+                    textSize: '30px',
+                    pathColor: `rgba(62, 152, 199, ${movie.vote_average / 10})`,
+                    textColor: '#f88',
+                    trailColor: '#d6d6d6',
+                    backgroundColor: '#3e98c7',
+                  })}
+                />
+              </div>
+              <p>Release Date: {movie.release_date}</p>
             </div>
-            
-          )}
+          ))}
         </div>
-        </Col>
-      </Row>
-    </Container>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <div className='d-flex justify-content-center m-5'>
+            <Button onClick={handleLoadMore} className={`${styles.loadMoreButton} text-center warning`} variant='warning'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+                <path fillRule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z"/>
+                <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466"/>
+              </svg>
+            </Button>
+          </div>
+        )}
+      </div>
     </>
   );
 };
